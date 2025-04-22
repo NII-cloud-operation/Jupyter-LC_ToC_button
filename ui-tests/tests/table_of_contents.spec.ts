186ffaef7f712b3cf2f6ae078b20026cdfdd453c
Taku Miyamoto <40813148+miyamoto83@users.noreply.github.com>
2025-04-23 00:34:45 +0900

    add notebook 7 test

--- ui-tests/tests/table_of_contents.spec.ts
+++ ui-tests/tests/table_of_contents.spec.ts
@@ -1,12 +1,49 @@
-import { expect, test } from '@jupyterlab/galata';
+import { IJupyterLabPage, expect, test } from '@jupyterlab/galata';
+import { Page, test as treeTest } from '@playwright/test';
+
+// https://github.com/jupyterlab/jupyterlab/blob/9844a6fdb680aeae28a4d6238433f751ce5a6204/galata/src/fixtures.ts#L319-L336
+async function waitForLabApplication({ baseURL }, use) {
+  const waitIsReady = async (
+    page: Page,
+    helpers: IJupyterLabPage
+  ): Promise<void> => {
+    await page.waitForSelector('#jupyterlab-splash', {
+      state: 'detached'
+    });
+    await helpers.waitForCondition(() => {
+      return helpers.activity.isTabActive('Launcher');
+    });
+    // Oddly current tab is not always set to active
+    if (!(await helpers.isInSimpleMode())) {
+      await helpers.activity.activateTab('Launcher');
+    }
+  };
+  await use(waitIsReady);
+}
+
+async function waitForTreeApplication(page: Page) {
+  await page.waitForSelector('.jp-FileBrowser-Panel', {
+    state: 'visible'
+  });
+}
+
+async function waitForNotebookApplication(page: Page) {
+  await page.waitForSelector('.jp-NotebookPanel:not(.lm-DockPanel-widget)', {
+    state: 'visible'
+  });
+}
 
 /**
  * Don't load JupyterLab webpage before running the tests.
  * This is required to ensure we capture all log messages.
  */
-test.use({ autoGoto: false });
-
-test('should emit an activation console message', async ({ page }) => {
+test.use({
+  autoGoto: false,
+  waitForApplication: waitForLabApplication
+});
+test('should emit an activation console message on JupyterLab', async ({
+  page
+}) => {
   const logs: string[] = [];
 
   page.on('console', message => {
@@ -22,19 +59,74 @@ test('should emit an activation console message', async ({ page }) => {
   ).toHaveLength(1);
 });
 
-test.use({ autoGoto: true });
-test('should work Table of Contents button', async ({ page }) => {
+treeTest(
+  'should emit an activation console message on Jupyter Notebook 7',
+  async ({ page }) => {
+    const logs: string[] = [];
+
+    page.on('console', message => {
+      logs.push(message.text());
+    });
+
+    await page.goto('http://localhost:8888/tree');
+    await waitForTreeApplication(page);
+
+    expect(
+      logs.filter(
+        s => s === 'JupyterLab extension table_of_contents is activated!'
+      )
+    ).toHaveLength(1);
+  }
+);
+
+test.use({
+  autoGoto: true,
+  waitForApplication: waitForLabApplication
+});
+test('should work Table of Contents button on JupyterLab', async ({ page }) => {
   // create new notebook
-  const fileName = 'table_of_contents_test.ipynb';
+  const fileName = 'table_of_contents_test1.ipynb';
   await page.notebook.createNew(fileName);
   await page.waitForSelector(`[role="main"] >> text=${fileName}`);
   // click Table of Contents button
-  await page
-    .locator('[data-command="table_of_contents:table-of-contents"]')
-    .click();
+  const tocButton = page.locator(
+    '[data-command="table_of_contents:table-of-contents"]'
+  );
+  await tocButton.click();
   // Table of Contents is visible
-  const tableOfContents = page
-    .locator('#jp-left-stack')
-    .locator('.jp-TableOfContents');
+  const tableOfContents = page.locator('#table-of-contents');
   await expect(tableOfContents).toBeVisible();
 });
+
+treeTest(
+  'should work Table of Contents button on Jupyter Notebook 7',
+  async ({ page }) => {
+    await page.goto('http://localhost:8888/tree');
+    await waitForTreeApplication(page);
+
+    // create new notebook and open new page in browser
+    const newButton = page.locator('div[data-jp-item-name="new-dropdown"]');
+    await newButton.click();
+    const [newPage] = await Promise.all([
+      page.context().waitForEvent('page'),
+      page.locator('li[data-command="notebook:create-new"]').click()
+    ]);
+    await waitForNotebookApplication(newPage);
+
+    // click Table of Contents button
+    const tocButton = newPage.locator(
+      '[data-command="table_of_contents:table-of-contents"]'
+    );
+    await tocButton.click();
+
+    // Table of Contents is visible
+    const tableOfContents = newPage.locator('#table-of-contents');
+    await expect(tableOfContents).toBeVisible();
+
+    // click Table of Contents button again
+    await tocButton.click();
+
+    // Table of Contents is hidden
+    await expect(tableOfContents).toBeHidden();
+  }
+);
